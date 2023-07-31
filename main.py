120f0edc71a84838c494315c32b14a376ade5c6a
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2023-07-31 22:04:46 +0900

    update

--- main.py
+++ main.py
@@ -20,12 +20,9 @@ print(model.check_model())
 
 ############# inference ################
 infer = VariableElimination(model)
-query = infer.query(variables=['Space_size'], evidence={'Cheating_indicator': 1})
-print(query)
+# query = infer.query(variables=['Space_size'], evidence={'Cheating_indicator': 1})
+# print(query)
 
-exit()
-############# Variables Estimation ################
-# print(WeightedMinFill(model).get_elimination_order(model.nodes))
 
 def mutual_information(variable1, variable2):
     query1 = infer.query(variables=[variable1], joint=True)
@@ -41,19 +38,27 @@ def mutual_information(variable1, variable2):
             mi += p_joint[i,j] * np.log(p_joint[i,j] / (p_variable1[i] * p_variable2[j]))
     return mi
 
-print('the mutual information between Space_Size and Cheating_Indicator', mutual_information('Space_Size', 'Cheating_Indicator'))
-print('the mutual information between Satisfaction_with_Reward and Cheating_Indicator', mutual_information('Satisfaction_with_Reward', 'Cheating_Indicator'))
-print('the mutual information between Season and Cheating_Indicator', mutual_information('Season', 'Cheating_Indicator'))
+# print('the mutual information between Space_size and Cheating_indicator', mutual_information('Space_size', 'Cheating_indicator'))
+# print('the mutual information between Chronic_stress and Cheating_indicator', mutual_information('Chronic_stress', 'Cheating_indicator'))
+# print('the mutual information between Amenity and Cheating_indicator', mutual_information('Amenity', 'Cheating_indicator'))
 
+mutual_info_list = []
+mutual_node_list = []
 for node in model.nodes():
-    if node == 'Cheating_Indicator':
+    if node == 'Cheating_indicator':
         continue
-    print('the mutual information between {0} and Cheating_Indicator'.format(node), mutual_information(node, 'Cheating_Indicator'))
+    mutual_info = mutual_information(node, 'Cheating_indicator')
+    mutual_node_list.append(node)
+    mutual_info_list.append(mutual_info)
 
+mutual_info_list, mutual_node_list = zip(*sorted(zip(mutual_info_list, mutual_node_list)))
+
+for node, mutual_info in zip(mutual_node_list, mutual_info_list):
+    print('mutual information==>\t', node, ':\t', mutual_info)
 
 ############# network plot ################
 import networkx as nx
 import pylab as plt
 nx_graph = nx.DiGraph(model.edges())
 nx.draw(nx_graph, with_labels=True, pos=nx.spring_layout(nx_graph), width = 1, font_size = 10, arrowsize = 10, node_size=500, node_color = 'skyblue')
-plt.show()
+plt.savefig("model.png")
